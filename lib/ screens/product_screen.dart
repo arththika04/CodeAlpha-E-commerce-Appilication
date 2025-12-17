f478326bc0c9b4deff4cc0f5e40a595317513a72
Srikantha_Arththika <arththika04@gmail.com>
2025-12-17 11:25:20 +0530

    add  dart

--- lib/ screens/product_screen.dart
+++ lib/ screens/product_screen.dart
@@ -1,6 +1,30 @@
-class Product {
-  final String name;
-  final double price;
+import 'package:flutter/material.dart';
+import '../data/products.dart';
+import 'cart_screen.dart';
 
-  Product(this.name, this.price);
+class ProductScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Products")),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(products[index].name),
+              subtitle: Text("\$${products[index].price}"),
+              trailing: Icon(Icons.add_shopping_cart),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => CartScreen()),
+                );
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
 }
